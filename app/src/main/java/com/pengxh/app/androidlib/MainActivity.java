6f2a41b577d55df402183c07470e199679c984bf
pengxianhong <290677893@qq.com>
2019-04-29 17:01:47 +0800

    添加log管理工具，自测试Pass

--- app/src/main/java/com/pengxh/app/androidlib/MainActivity.java
+++ app/src/main/java/com/pengxh/app/androidlib/MainActivity.java
@@ -1,17 +1,12 @@
 package com.pengxh.app.androidlib;
 
-import android.app.usage.NetworkStatsManager;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.pengxh.app.multilib.base.BaseNormalActivity;
-import com.pengxh.app.multilib.utils.NetworkStatsHelper;
-import com.pengxh.app.multilib.utils.TextUtil;
+import com.pengxh.app.multilib.utils.CrashHandler;
+import com.pengxh.app.multilib.utils.ToastUtil;
 
-@RequiresApi(api = Build.VERSION_CODES.M)
 public class MainActivity extends BaseNormalActivity {
 
     private static final String TAG = "MainActivity";
@@ -20,7 +15,7 @@ public class MainActivity extends BaseNormalActivity {
     @Override
     public void initView() {
         setContentView(R.layout.activity_main);
-        TextUtil.init(this);
+        CrashHandler.init(this);
     }
 
     @Override
@@ -30,22 +25,10 @@ public class MainActivity extends BaseNormalActivity {
 
     @Override
     public void initEvent() {
-        NetworkStatsHelper.hasPermissionToReadNetworkStatus(this);
-
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NetworkStatsManager statsManager = (NetworkStatsManager) getSystemService(NETWORK_STATS_SERVICE);
-                //获取每天的流量统计
-                if (statsManager == null) {
-                    Log.e(TAG, "statsManager is null, please check it");
-                } else {
-//                    long dayBytesWifi = NetworkStatsHelper.getDayBytesWifi(statsManager);
-                    long dayBytesMobile = NetworkStatsHelper.getDayBytesMobile(MainActivity.this, statsManager);
-                    String Mobiledata = NetworkStatsHelper.transformDataFlow(dayBytesMobile);
-
-                    TextUtil.showBeautifulToast("Total: " + Mobiledata + "MB", 3);
-                }
+                ToastUtil.showBeautifulToast("OK", 3);
             }
         });
     }
--- multilib/src/main/java/com/pengxh/app/multilib/utils/TextUtil.java
+++ multilib/src/main/java/com/pengxh/app/multilib/utils/TextUtil.java
@@ -1,14 +1,5 @@
 package com.pengxh.app.multilib.utils;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.os.Looper;
-import android.text.TextUtils;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.sdsmdg.tastytoast.TastyToast;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,72 +8,6 @@ import java.util.regex.Pattern;
  */
 
 public class TextUtil {
-
-    private static final String TAG = "TextUtil";
-    @SuppressLint("StaticFieldLeak")
-    private static Context mContext;
-
-    public static void init(Context mContext) {
-        TextUtil.mContext = mContext.getApplicationContext();//获取全局上下文，最长生命周期
-    }
-
-    /**
-     * 简单封装原生Toast
-     *
-     * @param msg     消息体
-     * @param isShort 是否短时间显示
-     */
-    public static void showToast(String msg, boolean isShort) {
-        if (!TextUtils.isEmpty(msg)) {
-            if (isShort) {
-                Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(mContext, msg, Toast.LENGTH_LONG).show();
-            }
-        } else {
-            Log.e(TAG, "msg must not be empty");
-        }
-    }
-
-    /**
-     * 简单封装Toast三方框架，高效果更炫，使用更简单
-     *
-     * @param msg
-     * @param level [0,5]
-     */
-    public static void showBeautifulToast(String msg, int level) {
-        if (!TextUtils.isEmpty(msg)) {
-            if (level > 5 || level < 0) {
-                Log.e(TAG, "Toast's level field is [0,5]");
-            } else {
-                switch (level) {
-                    case 0:
-                        TastyToast.makeText(mContext, msg, 0, TastyToast.DEFAULT).show();
-                        break;
-                    case 1:
-                        TastyToast.makeText(mContext, msg, 0, TastyToast.INFO).show();
-                        break;
-                    case 2:
-                        TastyToast.makeText(mContext, msg, 0, TastyToast.WARNING).show();
-                        break;
-                    case 3:
-                        TastyToast.makeText(mContext, msg, 0, TastyToast.SUCCESS).show();
-                        break;
-                    case 4:
-                        TastyToast.makeText(mContext, msg, 0, TastyToast.CONFUSING).show();
-                        break;
-                    case 5:
-                        TastyToast.makeText(mContext, msg, 0, TastyToast.ERROR).show();
-                        break;
-                    default:
-                        break;
-                }
-            }
-        } else {
-            Log.e(TAG, "msg must not be empty");
-        }
-    }
-
     /**
      * 匹配中文符号
      *
