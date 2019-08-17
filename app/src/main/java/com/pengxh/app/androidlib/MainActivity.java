be6ec51a5e5b13dce38a1f21948ed9e7002f33de
pengxianhong <290677893@qq.com>
2019-08-17 21:07:43 +0800

    添加sp帮助类

--- app/src/main/java/com/pengxh/app/androidlib/MainActivity.java
+++ app/src/main/java/com/pengxh/app/androidlib/MainActivity.java
@@ -4,7 +4,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.pengxh.app.multilib.base.BaseNormalActivity;
-import com.pengxh.app.multilib.utils.LogToFile;
+import com.pengxh.app.multilib.utils.SaveKeyValues;
 import com.pengxh.app.multilib.utils.ToastUtil;
 
 public class MainActivity extends BaseNormalActivity {
@@ -16,7 +16,7 @@ public class MainActivity extends BaseNormalActivity {
     public void initView() {
         setContentView(R.layout.activity_main);
         ToastUtil.init(this);
-        LogToFile.init(this, "");
+        SaveKeyValues.initSharedPreferences(this);
     }
 
     @Override
@@ -29,8 +29,8 @@ public class MainActivity extends BaseNormalActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SaveKeyValues.putValue("test", "key", "SaveKeyValues");
                 ToastUtil.showBeautifulToast("OK", ToastUtil.SUCCESS);
-                LogToFile.d(TAG, "OnClick");
             }
         });
     }
