d4db4bae08514bffb228f51d73b8436117f08078
pengxianhong <290677893@qq.com>
2021-01-12 21:01:53 +0800

    整合代码

--- app/src/main/java/com/pengxh/app/androidlib/MainActivity.java
+++ app/src/main/java/com/pengxh/app/androidlib/MainActivity.java
@@ -1,22 +1,18 @@
 package com.pengxh.app.androidlib;
 
-import android.app.Dialog;
 import android.util.Log;
-import android.view.View;
 
 import com.pengxh.app.multilib.base.BaseNormalActivity;
-import com.pengxh.app.multilib.widget.EasyToast;
-import com.pengxh.app.multilib.widget.dialog.MultiSelectBean;
-import com.pengxh.app.multilib.widget.dialog.MultiSelectDialog;
+import com.pengxh.app.multilib.widget.KeyBoardView;
 
-import java.util.ArrayList;
-import java.util.List;
+import butterknife.BindView;
 
-import butterknife.OnClick;
-
-public class MainActivity extends BaseNormalActivity implements View.OnClickListener {
+public class MainActivity extends BaseNormalActivity {
 
     private static final String TAG = "MainActivity";
+    @BindView(R.id.keyboardView)
+    KeyBoardView keyboardView;
+
 
     @Override
     public int initLayoutView() {
@@ -25,54 +21,21 @@ public class MainActivity extends BaseNormalActivity implements View.OnClickList
 
     @Override
     public void initData() {
+        keyboardView.setKeyboardClickListener(new KeyBoardView.KeyboardClickListener() {
+            @Override
+            public void onClick(String number) {
+                Log.d(TAG, "onClick: " + number);
+            }
 
+            @Override
+            public void onDelete() {
+
+            }
+        });
     }
 
     @Override
     public void initEvent() {
 
-    }
-
-    @OnClick({R.id.startView})
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.startView) {
-            new MultiSelectDialog.Builder()
-                    .setContext(this)
-                    .setTitle("请选择")
-                    .setData(getData())
-                    .setOutsideCancelable(false)
-                    .setNegativeButton("取消")
-                    .setPositiveButton("确定")
-                    .setOnDialogClickListener(new MultiSelectDialog.OnDialogClickListener() {
-                        @Override
-                        public void onConfirmClick(Dialog dialog, List<String> list) {
-                            if (list == null || list.size() == 0) {
-                                EasyToast.showToast("至少需要选中一条数据", EasyToast.WARING);
-                            } else {
-                                Log.d(TAG, "onConfirmClick: " + list);
-                                dialog.dismiss();
-                            }
-                        }
-
-                        @Override
-                        public void onCancelClick() {
-
-                        }
-                    })
-                    .build().show();
-        }
-    }
-
-    private List<MultiSelectBean> getData() {
-        List<MultiSelectBean> beanList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            MultiSelectBean bean = new MultiSelectBean();
-            bean.setPicture("https://img.zcool.cn/community/014f95573484436ac72580edfb370b.png@1280w_1l_2o_100sh.png");
-            bean.setTips("设备" + i);
-            bean.setDescription("11201909000" + i);
-            beanList.add(bean);
-        }
-        return beanList;
     }
 }
